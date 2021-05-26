0908bc7dec9f22866f270089b221cacf28649433
Oleg <ryasnoy.oleg@gmail.com>
2021-05-26 13:54:36 +0300

    update Keychainer

--- Tests/KeychainerTests/KeychainerTests.swift
+++ Tests/KeychainerTests/KeychainerTests.swift
@@ -1,11 +1,11 @@
     import XCTest
     @testable import Keychainer
-
+    
     final class KeychainerTests: XCTestCase {
         func testExample() {
             // This is an example of a functional test case.
             // Use XCTAssert and related functions to verify your tests produce the correct
             // results.
-            XCTAssertEqual(Keychainer().text, "Hello, World!")
+            XCTAssertEqual(Keychainer(serviceName: "Default").accessGroup, "Hello, World!")
         }
     }
